5bb1da87810b80d7bb335027b8a77239551bdc08
Cheng Shufen <48586255+ofiscarlett@users.noreply.github.com>
2023-11-20 13:56:15 +0200

    Update user_model.ts

--- parcelDelivery-user-backEnd/models/user_model.ts
+++ parcelDelivery-user-backEnd/models/user_model.ts
@@ -1,55 +1,89 @@
 import connection from "../dataBase";
 
-interface User {
-    firstName: string;
-    lastName: string;
-    email: string;
-    phone: string;
-    streetAddress: string;
-    postalCode: string;
-    city: string;
-    userName: string;
-    password: string;
-}
-
 const user = {
-    async createUser(userData: User) {
-        console.log(userData);
-        const query = `INSERT INTO user (
-            user_name, password, first_name, last_name, telephone, 
-            email, street_address, postal_code, city
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-
-        const result = await connection.promise().query(query, [
-            userData.userName, userData.password, userData.firstName, userData.lastName, 
-            userData.phone, userData.email, userData.streetAddress, userData.postalCode, userData.city
-        ]);
-
-        return result[0];
+    // Get all users
+    getUsers: async () => {
+        try {
+            const query = `SELECT * FROM user`;
+            const result = await connection.promise().query(query);
+            return result
+        }
+        catch (e: any) {
+            console.error(e.message);
+            return `Error from user model: ${e.message}`;
+        }
+    },
+    // Get user by id
+    getUser: async (userid: number) => {
+        try {
+            const query = `SELECT * FROM user WHERE id_user = ?`;
+            const result = await connection.promise().query(query, [userid]);
+            return result[0] as any;
+        }
+        catch (e: any) {
+            console.error(e.message);
+            return `Error from user model: ${e.message}`;
+        }
+    },
+    // Create user
+    createUser: async (
+        user_name:string, 
+        password:string, 
+        first_name:string, 
+        last_name:string, 
+        telephone:string, 
+        email:string, 
+        street_address:string, 
+        postal_code:string, 
+        city:string) => {
+        try {
+            const query = `INSERT INTO user (
+                user_name, password, first_name, last_name, telephone, 
+                email, street_address, postal_code, city) VALUES (?, ?, ?, ?, ?,?, ?, ?, ?)`;
+            const result = await connection.promise().query(query, [
+                user_name, password, first_name, last_name, telephone, email, street_address, postal_code, city]);
+            return result[0] as any;
+        }
+        catch (e: any) {
+            console.error(e.message);
+            return `Error from user model: ${e.message}`;
+        }
+    },
+    // Update user
+    updateUser: async (
+        userid: number, 
+        password:string, 
+        first_name:string, 
+        last_name:string, 
+        telephone:string, 
+        email:string, 
+        street_address:string, 
+        postal_code:string, 
+        city:string) => {
+        try {
+            const query = `UPDATE user SET first_name = ?, last_name=?, email = ?, password = ?, telephone = ?, Street_address = ? WHERE id_user = ?`;
+            const result = await connection.promise().query(query, [ password, first_name, last_name, telephone, email, street_address, postal_code, city, userid]);
+            return result[0] as any;
+        }
+        catch (e: any) {
+            console.error(e.message);
+            return `Error from user model: ${e.message}`;
+        }
+    },
+    // Delete user
+    deleteUser: async (userid: number) => {
+        try {
+            const query = `DELETE FROM user WHERE id_user = ?`;
+            const result = await connection.promise().query(query, [userid]);
+            return result[0] as any;
+        }
+        catch (e: any) {
+            console.error(e.message);
+            return `Error from user model: ${e.message}`;
+        }
     },
 
-    async getUser(userId: number) {
-        const query = `SELECT * FROM user WHERE id_user = ?`;
-        const [result] = await connection.promise().query(query, [userId]);
-        return result;
-    },
-
-    async deleteUser(userId: number) {
-        const query = `DELETE FROM user WHERE id_user = ?`;
-        const [result] = await connection.promise().query(query, [userId]);
-        return result;
-    },
-    async deleteUserbyName(user_name: string) {
-        const query = `DELETE FROM user WHERE user_name = ?`;
-        const [result] = await connection.promise().query(query, [user_name]);
-        return result;
-    },
-
-    async checkUserExists(userName: string) {
-        const query = `SELECT * FROM user WHERE user_name = ?`;
-        const [result] = await connection.promise().query(query, [userName]);
-        return {message: "Username already exists"};
-    }
 };
 
 export default user;
+
